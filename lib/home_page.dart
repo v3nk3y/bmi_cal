867956c3e2425e1c9ff7706b86c8a3bdde04e305
venkatesh <nagireddyvenkatesh@gmail.com>
2020-07-31 23:27:14 -0400

    Gender card select functionality added, Code Refactoring

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,6 +4,7 @@ import 'icon_content.dart';
 import 'reusable_card.dart';
 
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const homeBottomContainerColor = Color(0xFFEB1555);
 const homeBottomContainerHeight = 80.0;
 const maleCardIcon = FontAwesomeIcons.mars;
@@ -11,12 +12,18 @@ const femaleCardIcon = FontAwesomeIcons.venus;
 const maleCardText = 'MALE';
 const femaleCardText = 'FEMALE';
 
+enum Gender { Male, Female }
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  Color maleCardColor = Color(0xFF111328);
+  Color femaleCardColor = Color(0xFF111328);
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,20 +37,34 @@ class _HomePageState extends State<HomePage> {
               children: <Widget>[
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColor,
+                    colour: selectedGender == Gender.Male
+                        ? activeCardColor
+                        : inactiveCardColor,
                     cardChild: IconContent(
                       iconName: maleCardIcon,
                       iconLabel: maleCardText,
                     ),
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.Male;
+                      });
+                    },
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColor,
+                    colour: selectedGender == Gender.Female
+                        ? activeCardColor
+                        : inactiveCardColor,
                     cardChild: IconContent(
                       iconName: femaleCardIcon,
                       iconLabel: femaleCardText,
                     ),
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.Female;
+                      });
+                    },
                   ),
                 ),
               ],
